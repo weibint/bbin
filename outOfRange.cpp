167228c2134f7803066013ae96c9519fcdd61bb8
weibint <weibin.tan@ericsson.com>
2016-07-26 10:40:54 +0800

    add the file outOfRange.cpp

---
+++ outOfRange.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+#include <ctime>
+
+using namespace std;
+
+void Delay(int time)
+{
+	clock_t now = clock();
+	while(clock()-now < time);
+}
+
+int main()
+{
+	unsigned char test = 0;
+	int i =0;
+	while(1)
+	{
+		test = (unsigned char)i;
+		i++;
+		cout<<(int)test<<"||"<<i<<endl;
+		//Delay(2000);
+		//sleep(1);
+		if(i>1000)
+		{
+			cout<<"Reach 1000!"<<endl;
+			return 0;
+		}
+	}
+
+	return 0;
+}
