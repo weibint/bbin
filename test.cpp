5dea97fbc32b9350a350a9a8625d02a812793483
weibint <weibin.tan@ericsson.com>
2016-07-28 16:58:06 +0800

    update test.cpp file to check the int and float

--- test.cpp
+++ test.cpp
@@ -5,10 +5,15 @@ using namespace std;
 
 int main()
 {
-	cout<<(1?(5+3):(5+4))<<endl;
-	int m = -10;
-	int n;
-	n = m +9;
-	cout<<n<<endl;
+	float reported_in_equipment_delay_DL_in_pt_01chips_m = 5268;
+	int result0 = 0;
+	int result1 = 0;
+	int temp = 0;
+	temp = reported_in_equipment_delay_DL_in_pt_01chips_m/100 + 1;
+	result0 = (reported_in_equipment_delay_DL_in_pt_01chips_m/100 + 1)*100;
+	result1 = temp*100;
+
+	cout<<"result0:"<<result0<<endl;
+	cout<<"result1:"<<result1<<endl;
 	return 0;
 }
